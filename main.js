071f652c3176339342d8f4ca3ca6b3d0c58beff4
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-08-18 22:46:14 +0600

    search toggle added

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 import "./style.scss";
+document.addEventListener("DOMContentLoaded", function () {
+  const searchButton = document.getElementById("searchButton");
+  const crossButton = document.getElementById("crossButton");
+  const searchField = document.getElementById("searchField");
+  const customButtons = document.querySelectorAll(".custom-button");
+
+  searchButton.addEventListener("click", function () {
+    searchField.classList.toggle("visible");
+    searchButton.classList.toggle("hidden");
+    crossButton.classList.toggle("visible");
+    customButtons.forEach((button) => button.classList.toggle("hidden"));
+  });
+  crossButton.addEventListener("click", function () {
+    searchField.classList.toggle("visible");
+    searchButton.classList.toggle("hidden");
+    crossButton.classList.toggle("visible");
+    customButtons.forEach((button) => button.classList.toggle("hidden"));
+  });
+});
